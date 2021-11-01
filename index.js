bc77a88158c819909080820af8b52bc17504201b
sunxinyi0501 <sunxinyi@udel.edu>
2021-11-01 15:07:33 -0400

    done for exerice0-3

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    $( "#go" ).click(function(){
+        $("#jumbotron").append("<h1>" + $("#fname").val() + " " + $("#lname").val() + "</h1>");
+        return false;
     });
+});
